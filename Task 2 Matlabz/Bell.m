db19e9f4c3a6a454c521f6c925cc76fc0f7431b3
GRPlym56 <64791542+GRPlym56@users.noreply.github.com>
2021-11-23 20:30:25 +0000

    Responses plotted
    Sliders ditched for now, all responses successfully plotted on one graph, looking into variable centre frequency

--- Task 2 Matlabz/Bell.m
+++ Task 2 Matlabz/Bell.m
@@ -1,59 +1,14 @@
-Qfac = 2; %q factor
-Fs = 96000; %sampling frequency
-Fo = 7850; %centre frequency
-boost = 20; 
-k = 3*((boost -1)/(boost + 1));
+%plot frequency response of a discrete filter with sampling frequency 96Khz
+%centre frequency 7850 and Quality factor of 2
 
-FreqRange = [300 16000]; %band edge frequencies
-Wc = 2*pi*FreqRange(2); %convert upper cutoff to radians
-Wp = [tan((2*pi*FreqRange(1))/(2*Fs)) tan((2*pi*FreqRange(2))/(2*Fs))]; %prewarp cutoff frequencies
-Wo2 = Wp(1)*Wp(2); %calculate Wo2
-Wo =  2*pi*Fo;
+clc
+clear all
 
-Bw = (2*pi*(FreqRange(2) -FreqRange(1)))/Fs; %calculate bandwidth and convert to rads per sample
+Filtercalc(96000, 7850, 2)
 
 
-HSnum = [1 ((3+k)*(Wo/Qfac)) (Wo2)]; %continuous transfer function numerator
-HSden = [1 ((3-k)*(Wo/Qfac)) (Wo2)]; %continuous transfer function denominator
 
-disp(HSnum);
-disp(HSden);
 
-figure(1);
-[Ha, Wa] = freqs(HSnum, HSden); %plot continuous freq response
-Fa = Wa/(2*pi);
-plot(Fa, 20*log10(abs(Ha)))
-hold on
-
-figure(2);
-plot(Fa, angle(Ha)*180/pi); %plot continuous phase response
-hold on
-
-figure(1);
-[b1, a1] = bilinear(HSnum, HSden, Fs, Fo); %calc BZT coeffs
-[Hd, Fd] = freqz(b1, a1, 2^15, Fs);
-plot(Fd, 20*log10(abs(Hd)), 'r:') %Plot discrete BZT magnitude response
-
-figure(2);
-plot(Fd, angle(Hd)*180/pi, 'r:') %plot discrete BZT phase response
-
-figure(1);
-[b2, a2] = impinvar(HSnum, HSden, Fs); %calc imp. invar filter coeff
-[Hd, Fd] = freqz(b2, a2, 2^15, Fs);
-plot(Fd, 20*log10(abs(Hd)), 'g:') %plot imp. invar mag response
-axis([0 Fs 0 30])
-ylabel('Magnitude (dB)')
-xlabel('Frequency (Hz)')
-title('Analogue & Digital Filter Responses')
-legend('Analogue', 'BZT', 'Impulse Invariant');
-
-figure(2);
-plot(Fd, angle(Hd)*180/pi, 'g:') %impInvar phase resp
-axis([0 50000 -100 100])
-ylabel('Phase (Degrees)')
-xlabel('Frequency (Hz)')
-title('Analog & Digital Filter Responses')
-legend('Analog', 'BZT', 'Impulse Invariant');
 
 
 
--- Task 2 Matlabz/Filtercalc.m
+++ Task 2 Matlabz/Filtercalc.m
@@ -1,23 +1,60 @@
-function [HSnum, HSden, Fa, Fb] = Filtercalc(Fs, Fo, LowerCutoff, UpperCutoff, Qfac, boost)
-%FILTERCALC Summary of this function goes here
-%   Detailed explanation goes here
+function Filtercalc(Fs, Fo, Qfac) %inputs are sampling frequency, centre frequency and quality factor
+%function create a digital bell filter based on frequency and quality
+%factor inputs
+%plots the magnitude response of the filter at different boost/cut levels
+%between -20 and 20dB in 4dB steps
 
-k = 3*((boost -1)/(boost + 1));
+boost = -20:4:20; %boost/cut range
+L = length(boost); %calculate vector length
 
-FreqRange = [LowerCutoff UpperCutoff]; %band edge frequencies
-Wc = 2*pi*FreqRange(2); %convert upper cutoff to radians
-Wp = [tan((2*pi*FreqRange(1))/(2*Fs)) tan((2*pi*FreqRange(2))/(2*Fs))]; %prewarp cutoff frequencies
-Wo2 = Wp(1)*Wp(2); %calculate Wo2
-Wo =  2*pi*Fo;
+G = zeros(1, L); %preallocate gain vector
+k = zeros(1, L); %preallocate k vector
 
-Bw = (2*pi*(FreqRange(2) -FreqRange(1)))/Fs; %calculate bandwidth and convert to rads per sample
+for c = 1:L %repeat for length of boost vector (number of steps between -20 and 20)
+    G(c) = 10^(boost(c)/20); %calculate gain
+    k(c) = 3*((G(c) -1)/(G(c) + 1)); %calculate k
+end
 
-HSnum = [1 ((3+k)*(Wo/Qfac)) (Wo2)]; %continuous transfer function numerator
-HSden = [1 ((3-k)*(Wo/Qfac)) (Wo2)]; %continuous transfer function denominator
+%prealloacate for speed
+Wo = zeros(1, L);
+Wo2 = zeros(1, L);
+HSnum = zeros(L,3);
+HSden = zeros(L,3);
 
-[Ha, Wa] = freqs(HSnum, HSden); %continuous freq response
-Fa = Wa/(2*pi);
-Fb = 20*log10(abs(Ha));
+b = zeros(L,3);
+a = zeros(L,3);
+
+for i = 1:L %repeat for number of steps
+    
+    Wo(i) =  2*pi*Fo; %calculate centre frequency in Radians per second
+    Wo2(i) = Wo(i)^2; %centre frequency in Rads/s squared
+
+    HSnum(i,:) = [1 ((3+k(i))*(Wo(i)/Qfac)) (Wo2(i))]; %calculate continuous transfer function numerator
+    HSden(i,:) = [1 ((3-k(i))*(Wo(i)/Qfac)) (Wo2(i))]; %calculate continuous transfer function denominator
+     
+    [b(i,1:3), a(i,1:3)] = bilinear(HSnum(i,1:3), HSden(i,1:3), Fs, Fo); %calc BZT coefficients for each iteration of the continuous TF
+
+end
+
+figure(1); %create plot figure for the frequency response
+clf
+ylabel('Magnitude (dB)')
+xlabel('Frequency (Hz)')
+axis([0 Fs -20 20]); %x-axis from 0 to 96Khz, y-axis from -20 to dB
+grid on;
+hold on
+
+%preallocate matrices for discrete frequency response
+Hd = zeros(32768, L); 
+Fd = zeros(32768, L);
+
+for x = 1:L %repeat for number of steps
+    [Hd(:,x), Fd(:,x)] = freqz(b(x,:), a(x,:), 2^15, Fs); %get frequency response of discrete BZT transfer function
+    %Hd: magnitude component
+    %Fd: frequency component
+    plot(Fd(:,x), 20*log10(abs(Hd(:,x))), 'b') %Plot discrete BZT magnitude response
+end
+
 
 
 end
