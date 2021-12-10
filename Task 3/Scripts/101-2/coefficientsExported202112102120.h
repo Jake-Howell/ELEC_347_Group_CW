116f8ca5174b6beca3eadfffd42fcbf6b9d9a162
Mircea-Iuliu Micle <83925805+Mircerson@users.noreply.github.com>
2021-12-10 21:23:02 +0000

    101-2 # Rev 2
    Added serial port
    Added export

---
+++ Task 3/Scripts/101-2/coefficientsExported202112102120.h
@@ -0,0 +1,17 @@
+//Coefficients exported
+//Generated using 101-2 app
+//ELEC347 - MMI - 2021
+
+#define Fs 96000
+#define CentreFrequency 1250
+
+//A-Denominator coefficients
+float a1 = 1;
+float a2 = -1.9091;
+float a3 = 0.91549;
+//B-Nominator coefficients
+float b1 = 1.0329;
+float b2 = -1.9091;
+float b3 = 0.8826;
+
+Written to serial:A,1,-1.9091,0.91549,B,1.0329,-1.9091,0.8826,STOP
---
+++ Task 3/Scripts/101-2/coefficientsExported20211210218.h
@@ -0,0 +1,17 @@
+//Coefficients exported
+//Generated using 101-2 app
+//ELEC347 - MMI - 2021
+
+#define Fs 96000
+#define CentreFrequency 125
+
+//A-Denominator coefficients
+float a1 = 1;
+float a2 = -1.9941;
+float a3 = 0.99413;
+//B-Nominator coefficients
+float b1 = 1.0023;
+float b2 = -1.9941;
+float b3 = 0.99184;
+
+Written to serial:A,1,-1.9941,0.99413,B1.0023,-1.9941,0.99184,STOP
