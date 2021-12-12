c44af9dd2a500d17d016bae38a5806cb02915a52
Mircea-Iuliu Micle <83925805+Mircerson@users.noreply.github.com>
2021-12-12 15:14:17 +0000

    Report is coming
    Added plots
    More testing on 101-2

---
+++ Task 3/Scripts/101-2/coefficientsExported202112112240.h
@@ -0,0 +1,17 @@
+//Coefficients exported
+//Generated using 101-2 app
+//ELEC347 - MMI - 2021
+
+#define Fs 96000
+#define CentreFrequency 125
+
+//A-Denominator coefficients
+float a1 = 1;
+float a2 = -1.9955;
+float a3 = 0.99555;
+//B-Nominator coefficients
+float b1 = 1.02;
+float b2 = -1.9955;
+float b3 = 0.97551;
+
+Written to serial:A,1,-1.9955,0.99555,B,1.02,-1.9955,0.97551,STOP
---
+++ Task 3/Scripts/101-2/coefficientsExported202112112241.h
@@ -0,0 +1,17 @@
+//Coefficients exported
+//Generated using 101-2 app
+//ELEC347 - MMI - 2021
+
+#define Fs 96000
+#define CentreFrequency 125
+
+//A-Denominator coefficients
+float a1 = 1;
+float a2 = -1.9984;
+float a3 = 0.99851;
+//B-Nominator coefficients
+float b1 = 1.0067;
+float b2 = -1.9984;
+float b3 = 0.99182;
+
+Written to serial:A,1,-1.9984,0.99851,B,1.0067,-1.9984,0.99182,STOP
